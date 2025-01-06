b5c1d42aa9e7d9378db2a88c2d8c2ee8e7023a95
Dookie_Loserr <dookie_loserr@hotmail.com>
2025-01-05 20:27:45 -0500

    Spring AI Functions

--- src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
@@ -1,16 +1,16 @@
 package guru.springframework.springaifunctions.services;
 
 
+import guru.springframework.springaifunctions.functions.StockQuoteFunction;
 import guru.springframework.springaifunctions.functions.WeatherServiceFunction;
-import guru.springframework.springaifunctions.model.Answer;
-import guru.springframework.springaifunctions.model.Question;
-import guru.springframework.springaifunctions.model.WeatherRequest;
+import guru.springframework.springaifunctions.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.model.ModelOptionsUtils;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -48,13 +48,41 @@ public class OpenAIServiceImpl implements OpenAIService {
                            .function("CurrentWeather",new WeatherServiceFunction(apiNinjasKey))
                            .description("Get the current weather for a location")
                            .inputType(WeatherRequest.class)
-                       .build()))
+                           .responseConverter(response -> {
+                               String schema = ModelOptionsUtils.getJsonSchema(WeatherResponse.class, false);
+                               String json = ModelOptionsUtils.toJsonString(response);
+                               return schema + "\n" + json;
+                           })
+                     .build()))
                .build();
 
 
         Message userMessage = new PromptTemplate(question.question()).createMessage();
 
         var response = openAiChatModel.call(new Prompt(List.of(systemMessage,userMessage), promptOptions));
+        return new Answer(response.getResult().getOutput().getContent());
+    }
+
+    @Override
+    public Answer getStockPrice(Question question) {
+        var promptOptions = OpenAiChatOptions.builder()
+                .functionCallbacks(List.of(FunctionCallback.builder()
+                        .function("CurrentStockPrice", new StockQuoteFunction(apiNinjasKey))
+                        .description("Get the current stock price for a stock symbol")
+                                .inputType(StockPriceRequest.class)
+                        .responseConverter((response) -> {
+                            String schema = ModelOptionsUtils.getJsonSchema(StockPriceResponse.class, false);
+                            String json = ModelOptionsUtils.toJsonString(response);
+                            return schema + "\n" + json;
+                        })
+                        .build()))
+                .build();
+
+        Message userMessage = new PromptTemplate(question.question()).createMessage();
+        Message systemMessage = new SystemPromptTemplate("You are an agent which returns back a stock price for the given stock symbol (or ticker) include the last updated epoch time, convert epoch time to DateTime").createMessage();
+
+        var response = openAiChatModel.call(new Prompt(List.of(userMessage, systemMessage), promptOptions));
+
         return new Answer(response.getResult().getOutput().getContent());
     }
 }
